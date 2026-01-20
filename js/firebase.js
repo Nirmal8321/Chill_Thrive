25279824e66ffcd60c8293158b89203c8f130b23
Nirmal <nirmalprajapati2006@gmail.com>
2026-01-20 21:16:42 +0530

    Version 1.1

--- js/firebase.js
+++ js/firebase.js
@@ -1,15 +1,26 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getFirestore, collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { 
+    getFirestore, 
+    collection, 
+    addDoc, 
+    getDocs, 
+    deleteDoc, 
+    doc, 
+    updateDoc, 
+    setDoc,
+    getDoc  // <--- MAKE SURE THIS IS HERE!
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "XXXX",
-  appId: "XXXX"
+    apiKey: "AIzaSyDT2G4Fd0HhSBo7Ie1m0sF8c7-CNLOV7Ho",
+    authDomain: "giggle-gird-gwoc-2026.firebaseapp.com",
+    projectId: "giggle-gird-gwoc-2026",
+    storageBucket: "giggle-gird-gwoc-2026.appspot.com",
+    messagingSenderId: "18442374008",
+    appId: "1:18442374008:web:89f62a6721c94377c6968d"
 };
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export { collection, addDoc, getDocs };
+const db = getFirestore(app);
+
+export { db, collection, addDoc, getDocs, deleteDoc, doc, updateDoc, setDoc, getDoc };
